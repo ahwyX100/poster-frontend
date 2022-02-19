7eec10b30b07506b1f924f127bc7c50845512e42
Leon Chen <ahwyx100@163.com>
2022-02-19 10:26:53 +0800

    First commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,14 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
   overrides: [
     {
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,22 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
+
+export interface UserProps {
+  isLogin: boolean;
+  userName?: string;
+}
+
+export interface TemplateProps {
+  id: number;
+  title: string;
+  coverImg: string;
+  author: string;
+  copiedCount: number;
+}
+
+export interface GlobalDataProps {
+  user: UserProps;
+  templates: TemplateProps[];
+}
 
 export default createStore({
   state: {},
